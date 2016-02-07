e827045b9fb14ae7da063d2f89fba01e84519656
Bogdan Buduroiu <bbuduroiu@gmail.com>
2016-02-07 14:08:57 +0000

    Added Getter for Pubs

--- app/src/main/java/com/asherfischbaum/hackapint/PubMatcher.java
+++ app/src/main/java/com/asherfischbaum/hackapint/PubMatcher.java
@@ -31,12 +31,6 @@ import java.util.Map;
  * Created by bogdanbuduroiu on 06/02/2016.
  */
 public class PubMatcher extends AsyncTask<Void, Void, Void> {
-    private final String LOG_TAG = this.getClass().getSimpleName();
-    final String MPS_RESULTS = "results";
-    final String MPS_ADDR = "formatted_address";
-    final String MPS_GEOM = "geometry";
-    final String MPS_LOC = "location";
-    final String MPS_NAME = "name";
 
     private Double lat;
     private Double lng;
@@ -145,31 +139,15 @@ class Pub {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Double getLat() {
         return lat;
-    }
-
-    public void setLat(Double lat) {
-        this.lat = lat;
     }
 
     public Double getLng() {
         return lng;
     }
 
-    public void setLng(Double lng) {
-        this.lng = lng;
-    }
-
     public String getVicinity() {
         return vicinity;
-    }
-
-    public void setVicinity(String vicinity) {
-        this.vicinity = vicinity;
     }
 }
